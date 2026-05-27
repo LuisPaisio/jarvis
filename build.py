33fc3a31e4264bba453127fb13db13c066276746
Luis <luispaisio1@gmail.com>
2026-05-27 14:30:10 +0000

    fix cuda library 2

--- build.py
+++ build.py
@@ -1,18 +1,12 @@
 import os
 import sys
 import subprocess
-import importlib.util
 
 ICON = "jarvis.ico"
 ENTRY_POINT = os.path.join("src", "tray.py")
 OUTPUT_NAME = "Jarvis"
 
 sep = ";" if sys.platform == "win32" else ":"
-
-spec_cublas = importlib.util.find_spec('nvidia.cublas')
-spec_cudnn = importlib.util.find_spec('nvidia.cudnn')
-cublas_lib = os.path.join(spec_cublas.submodule_search_locations[0], 'lib')
-cudnn_lib = os.path.join(spec_cudnn.submodule_search_locations[0], 'lib')
 
 BUILD_ARGS = [
     "pyinstaller",
@@ -22,8 +16,6 @@ BUILD_ARGS = [
     "--collect-all", "nvidia.cublas",
     "--collect-all", "nvidia.cudnn",
     "--hidden-import", "keyboard",
-    "--add-data", f"{os.path.join(cublas_lib, '*.dll')}{sep}nvidia{os.sep}cublas{os.sep}lib",
-    "--add-data", f"{os.path.join(cudnn_lib, '*.dll')}{sep}nvidia{os.sep}cudnn{os.sep}lib",
     f"--name={OUTPUT_NAME}",
     "--add-data", f"jarvis.ico{sep}.",
     ENTRY_POINT,
